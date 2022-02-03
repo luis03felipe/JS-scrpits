792c1abdc48890d1c04781b25e0de7c0352fc673
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-03 09:30:52 -0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,17 @@
+/*
+ Number
+    *números
+
+    22 - Numeros inteiros
+    12.5 - Numeros Reais/Float
+    NaN - Not a Number
+    Infinity - Infinito
+
+    */
+
+    console.log(22)
+    console.log(-22)
+    console.log(12.5)
+
+    console.log(12.5 + 22)
+    
