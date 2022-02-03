5fe6689363cea9021fef60f2611ff3f78cfa85a6
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-03 09:10:27 -0300

    Como usar aspas e crase.
    Aspas duplas e aspas comuns pode-se escrever algo dentro.
    console.log("aaa")
    console.log('bbb')
    Já a crase abre possibilidades para operações matematicas e espaços entre linhas. 
    console.log(`${33+33}`)
    console.log(`a 
    b 
    b`)

---
+++ uso de aspas.js
@@ -0,0 +1,20 @@
+/* Aspas duplas e aspas comuns pode-se escrever algo dentro.
+console.log("aaa")
+console.log('bbb')
+Já a crase abre possibilidades para operações matematicas e espaços entre linhas. 
+console.log(`${33+33}`)
+console.log(`a 
+b 
+b`)
+*/
+
+console.log("Olá Luís") //"" Aspas duplas
+console.log('Olá lulu') //'' Aspas comuns
+console.log(`${22+22}`) //`` Template literals ou Template strings (crase)
+console.log(`a 
+a
+a
+`)
+
+console.log(2+2) // uma experessão numérica 
+console.log(`${22+22}`) //possibilidade de colocar valores se chama interpolação
