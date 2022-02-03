727e3fdba78dc1245a0af25305d90422a1589e76
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-03 09:39:53 -0300

    true or false

---
+++ boolean.js
@@ -0,0 +1,15 @@
+/*
+ Boolean
+    somente dois valore
+
+    true - verdadeiro
+    false - falso
+    */
+    
+    console.log(true)
+    console.log(false)
+
+// ambos são muito utilizados e necessários para a lógica de programação;
+
+/* Em outras palavras; Tenho uma xicara, ela está suja ou não? true or false.
+Se estiver eu lavo (true), se não estiver, não preciso lavar (false)*/
