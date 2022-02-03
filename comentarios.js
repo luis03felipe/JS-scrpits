63b035f5b19f63b31c266b845bd65c0c090e887b
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-03 09:05:56 -0300

    A
    a

---
+++ comentarios.js
@@ -0,0 +1,8 @@
+/* COMENTÁRIOS:
+ O código abaixo escreve alguma mensagem no console (devtools).
+ No meu caso é em Sources*/
+
+ // usar duas barras ou barra e asterisco, no final o contrário, asterisco barra
+
+console.log('Primeiro Código')
+console.log('o contrário')
