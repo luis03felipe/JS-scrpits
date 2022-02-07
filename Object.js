ba7121fb7cdb62e247eadf04bfd4f10dd2b424be
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-07 10:02:15 -0300

    Object

---
+++ Object.js
@@ -0,0 +1,15 @@
+/* Obect
+    - Objeto
+    - Propriedades/Atributos
+    - Funcionalidades/Métodos
+
+    {propriedade: "valor"}
+    */
+
+console.log({
+    name: "Luís",
+    Idade: 18,
+    andar: function() {
+        console.log('andar')
+    }
+})
