c258f586cbd97541cb52405505c3471bd8bbf6af
Luís Felipe <98954558+luis03felipe@users.noreply.github.com>
2022-02-07 10:52:42 -0300

    array
    Array (Vetores)
    - Uma lista
    - Agrupamento de dado

---
+++ array.js
@@ -0,0 +1,20 @@
+/*
+Array (Vetores)
+    - Uma lista
+    - Agrupamento de dado
+
+    ["Luís", 18]
+    */
+
+    console.log(["Luís", 18])
+
+    console.log([
+        "Ovos",
+        "Leite",
+        "Frango",
+        "Carne",
+        1,
+        2,
+        3,
+        4
+    ])
